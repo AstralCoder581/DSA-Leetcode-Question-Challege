fa868da9f6296a2b19b912ce68aacf8a3a179dbf
Abhishek V K <160398136+AstralCoder581@users.noreply.github.com>
2024-07-30 17:38:59 +0530

    Create Merge_Sort.java

---
+++ Abhishek_V_K/Lab_Programs_java/Merge_Sort.java
@@ -0,0 +1,53 @@
+public class mergesortProg{
+	public static void printArr(int arr[]){
+		for (int i=0; i < arr.length; i++){
+			System.out.print(arr[i]+" ");
+		}
+		System.out.println();
+	}
+
+	public static void mergesort(int arr[], int si, int ei){
+		if(si >= ei){
+			return;
+		}
+		int mid = si + (ei-si)/2;
+		mergesort(arr,si,mid);
+		mergesort(arr,mid+1,ei);
+		merge(arr,si,mid,ei);	
+	}
+
+	public static void merge(int arr[], int si, int mid, int ei){
+		int temp[] = new int[ei-si+1];
+		int i = si;
+		int j = mid+1;
+		int k = 0;
+
+		while(i <= mid && j <= ei){
+			if(arr[i] < arr[j]){
+				temp[k] = arr[i];
+				i++;
+			} else{
+				temp[k] = arr[j];
+				j++;
+			}
+			k++;
+		}
+
+		while(i <= mid){
+			temp[k++] = arr[i++];
+		}
+		while(j <= ei){
+			temp[k++] = arr[j++];
+		}
+		
+		for( k= 0, i = si; k < temp.length; k++, i++){
+			arr[i] = temp[k];
+		}
+	}
+
+	public static void main(String args[]){
+		int arr[] = {5,9,8,6,7,3};
+		mergesort(arr,0,arr.length-1);
+		printArr(arr);
+	}
+}
