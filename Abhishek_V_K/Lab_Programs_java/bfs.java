adc962f958bb56d39d727621fb8fe940e3ba8311
Abhishek V K <160398136+AstralCoder581@users.noreply.github.com>
2024-08-01 20:15:27 +0530

    Create bfs.java

---
+++ Abhishek_V_K/Lab_Programs_java/bfs.java
@@ -0,0 +1,71 @@
+import java.util.*;
+
+public class bfsProgram {
+    static class Edge{
+        int src;
+        int dest;
+
+        public Edge(int s, int d){
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge> graph[]){
+        for (int i=0; i<graph.length; i++){
+            graph[i] = new ArrayList<Edge>();
+        }
+
+        graph[0].add(new Edge(0,1));
+        graph[0].add(new Edge(0,2));
+
+        graph[1].add(new Edge(1,0));
+        graph[1].add(new Edge(1,3));
+
+        graph[2].add(new Edge(2,0));
+        graph[2].add(new Edge(2,4));
+
+        graph[3].add(new Edge(3,1));
+        graph[3].add(new Edge(3,4));
+        graph[3].add(new Edge(3,5));
+
+        graph[4].add(new Edge(4,2));
+        graph[4].add(new Edge(4,3));
+        graph[4].add(new Edge(4,5));
+
+        graph[5].add(new Edge(5,3));
+        graph[5].add(new Edge(5,4));
+        graph[5].add(new Edge(5,6));
+
+        graph[6].add(new Edge(6,5));
+    }
+
+    public static void bfs(ArrayList<Edge> graph[], int V){
+        Queue<Integer> q = new LinkedList<>();
+        boolean vis[] = new boolean[V];
+        q.add(0);
+
+        while (!q.isEmpty()) {
+            int curr = q.remove();
+            if (vis[curr]==false) {
+                System.out.print(curr+" ");
+                vis[curr] = true;
+                
+                for(int i=0; i<graph[curr].size(); i++){
+                    Edge e = graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }           
+        }
+    }
+
+    public static void main(String[] args) {
+        int V=7;
+
+        @SuppressWarnings("unchecked")
+        ArrayList<Edge> graph[] = new ArrayList[V];
+        createGraph(graph);
+        bfs(graph, V);
+        System.out.println();
+    }
+}
