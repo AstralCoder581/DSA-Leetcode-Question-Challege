910638384f3423a9270e06802a06b01b910c83e9
Abhishek V K <160398136+AstralCoder581@users.noreply.github.com>
2024-08-06 00:51:57 +0530

    Update knapsack_Algorithm.java

--- Abhishek_V_K/Lab_Programs_java/knapsack_Algorithm.java
+++ Abhishek_V_K/Lab_Programs_java/knapsack_Algorithm.java
@@ -1,29 +1,17 @@
-public class knapsack { 
-
-    static int max(int a, int b){ 
-        return (a > b)? a : b; 
-    }
+public class knapsack{ 
     static int knapSack(int W, int wt[], int val[], int n){
-        int i, w;
         int K[][] = new int[n+1][W+1];
-        for (i = 0; i <= n; i++){
-            for (w = 0; w <= W; w++){
-                if (i==0 || w==0)
-                    K[i][w] = 0;
-                else if (wt[i-1] <= w)
-                    K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]);
-                else
-                    K[i][w] = K[i-1][w];
+        for (int i = 0; i <= n; i++){
+            for (int w = 0; w <= W; w++){
+                if (i==0 || w==0) K[i][w] = 0;
+                else if (wt[i-1] <= w) K[i][w] = Math.max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w]);
+                else K[i][w] = K[i-1][w];
             }
-        }
-        return K[n][W];
+        }return K[n][W];
     }
-    public static void main(String args[]) 
-    { 
-        int profit[] = new int[] { 60, 100, 120 }; 
-        int weight[] = new int[] { 10, 20, 30 }; 
-        int W = 50; 
-        int n = profit.length; 
-        System.out.println(knapSack(W, weight, profit, n)); 
+    public static void main(String args[]){ 
+        int weight[] = new int[] {3, 5, 6, 9};
+        int profit[] = new int[] {10, 4, 9, 11};   
+        System.out.println(knapSack(7, weight, profit, profit.length)); 
     } 
 } 
