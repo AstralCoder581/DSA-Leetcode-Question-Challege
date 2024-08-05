05f4da471dcb08e61dd6f719ae87242b6898295b
Abhishek V K <160398136+AstralCoder581@users.noreply.github.com>
2024-08-06 00:01:15 +0530

    Update kruskal's_Algorithm.java

--- Abhishek_V_K/Lab_Programs_java/kruskal's_Algorithm.java
+++ Abhishek_V_K/Lab_Programs_java/kruskal's_Algorithm.java
@@ -1,134 +1,81 @@
-import java.io.*;
 import java.util.*;
 
-
-// User function Template for Java
-
-class DisjointSet {
-    List<Integer> rank = new ArrayList<>();
-    List<Integer> parent = new ArrayList<>();
-    List<Integer> size = new ArrayList<>();
-    public DisjointSet(int n) {
-        for (int i = 0; i <= n; i++) {
-            rank.add(0);
-            parent.add(i);
-            size.add(1);
+public class KruskalsAlgo {
+    static class Edge implements Comparable<Edge> {
+        int src, dest, weight;
+        Edge(int src, int dest, int weight) {
+            this.src = src;
+            this.dest = dest;
+            this.weight = weight;
+        }
+        public int compareTo(Edge other) {
+            return this.weight - other.weight;
         }
     }
 
-    public int findUPar(int node) {
-        if (node == parent.get(node)) {
-            return node;
-        }
-        int ulp = findUPar(parent.get(node));
-        parent.set(node, ulp);
-        return parent.get(node);
-    }
-
-    public void unionByRank(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (rank.get(ulp_u) < rank.get(ulp_v)) {
-            parent.set(ulp_u, ulp_v);
-        } else if (rank.get(ulp_v) < rank.get(ulp_u)) {
-            parent.set(ulp_v, ulp_u);
-        } else {
-            parent.set(ulp_v, ulp_u);
-            int rankU = rank.get(ulp_u);
-            rank.set(ulp_u, rankU + 1);
+    static class Subset {
+        int parent, rank;
+        Subset(int parent) {
+            this.parent = parent;
+            this.rank = 0;
         }
     }
 
-    public void unionBySize(int u, int v) {
-        int ulp_u = findUPar(u);
-        int ulp_v = findUPar(v);
-        if (ulp_u == ulp_v) return;
-        if (size.get(ulp_u) < size.get(ulp_v)) {
-            parent.set(ulp_u, ulp_v);
-            size.set(ulp_v, size.get(ulp_v) + size.get(ulp_u));
-        } else {
-            parent.set(ulp_v, ulp_u);
-            size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+    static int find(Subset[] subsets, int i) {
+        if (subsets[i].parent != i)
+            subsets[i].parent = find(subsets, subsets[i].parent);
+        return subsets[i].parent;
+    }
+
+    static void union(Subset[] subsets, int x, int y) {
+        int rootX = find(subsets, x);
+        int rootY = find(subsets, y);
+
+        if (subsets[rootX].rank < subsets[rootY].rank)
+            subsets[rootX].parent = rootY;
+        else if (subsets[rootX].rank > subsets[rootY].rank)
+            subsets[rootY].parent = rootX;
+        else {
+            subsets[rootY].parent = rootX;
+            subsets[rootX].rank++;
         }
     }
-}
-class Edge implements Comparable<Edge> {
-    int src, dest, weight;
-    Edge(int _src, int _dest, int _wt) {
-        this.src = _src; this.dest = _dest; this.weight = _wt;
-    }
-    // Comparator function used for
-    // sorting edgesbased on their weight
-    public int compareTo(Edge compareEdge) {
-        return this.weight - compareEdge.weight;
-    }
-};
-class Solution {
-    //Function to find sum of weights of edges of the Minimum Spanning Tree.
-    static int spanningTree(int V,
-                            ArrayList<ArrayList<ArrayList<Integer>>> adj) {
-        List<Edge> edges = new ArrayList<Edge>();
-        // O(N + E)
-        for (int i = 0; i < V; i++) {
-            for (int j = 0; j < adj.get(i).size(); j++) {
-                int adjNode = adj.get(i).get(j).get(0);
-                int wt = adj.get(i).get(j).get(1);
-                int node = i;
-                Edge temp = new Edge(i, adjNode, wt);
-                edges.add(temp);
-            }
-        }
-        DisjointSet ds = new DisjointSet(V);
-        // M log M
+
+    public static void kruskalMST(List<Edge> edges, int V) {
         Collections.sort(edges);
-        int mstWt = 0;
-        // M x 4 x alpha x 2
-        for (int i = 0; i < edges.size(); i++) {
-            int wt = edges.get(i).weight;
-            int u = edges.get(i).src;
-            int v = edges.get(i).dest;
+        Subset[] subsets = new Subset[V + 1];
+        for (int v = 1; v <= V; v++)
+            subsets[v] = new Subset(v);
 
-            if (ds.findUPar(u) != ds.findUPar(v)) {
-                mstWt += wt;
-                ds.unionBySize(u, v);
+        List<Edge> result = new ArrayList<>();
+        for (Edge edge : edges) {
+            int x = find(subsets, edge.src);
+            int y = find(subsets, edge.dest);
+            if (x != y) {
+                result.add(edge);
+                union(subsets, x, y);
+                if (result.size() == V - 1)
+                    break;
             }
         }
 
-        return mstWt;
+        System.out.println("MST: ");
+        for (Edge edge : result)
+            System.out.println(edge.src + " -- " + edge.dest + " == " + edge.weight);
     }
-}
 
-class Main {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         int V = 5;
-        ArrayList<ArrayList<ArrayList<Integer>>> adj = new ArrayList<ArrayList<ArrayList<Integer>>>();
-        int[][] edges =  {{0, 1, 2}, {0, 2, 1}, {1, 2, 1}, {2, 3, 2}, {3, 4, 1}, {4, 2, 2}};
+        List<Edge> edges = Arrays.asList(
+            new Edge(1, 5, 5),
+            new Edge(1, 2, 10),
+            new Edge(2, 3, 1),
+            new Edge(2, 4, 6),
+            new Edge(3, 4, 2),
+            new Edge(4, 5, 3),
+            new Edge(5, 3, 7)
+        );
 
-        for (int i = 0; i < V; i++) {
-            adj.add(new ArrayList<ArrayList<Integer>>());
-        }
-
-        for (int i = 0; i < 6; i++) {
-            int u = edges[i][0];
-            int v = edges[i][1];
-            int w = edges[i][2];
-
-            ArrayList<Integer> tmp1 = new ArrayList<Integer>();
-            ArrayList<Integer> tmp2 = new ArrayList<Integer>();
-            tmp1.add(v);
-            tmp1.add(w);
-
-            tmp2.add(u);
-            tmp2.add(w);
-
-            adj.get(u).add(tmp1);
-            adj.get(v).add(tmp2);
-        }
-
-        Solution obj = new Solution();
-        int mstWt = obj.spanningTree(V, adj);
-        System.out.println("The sum of all the edge weights: " + mstWt);
-
+        kruskalMST(edges, V);
     }
 }
