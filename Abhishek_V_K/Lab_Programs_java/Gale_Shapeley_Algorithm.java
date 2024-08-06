6ce2c9de19120fdb5591bf42f41347cf0da60649
Abhishek V K <160398136+AstralCoder581@users.noreply.github.com>
2024-08-06 11:48:22 +0530

    Update Gale_Shapeley_Algorithm.java

--- Abhishek_V_K/Lab_Programs_java/Gale_Shapeley_Algorithm.java
+++ Abhishek_V_K/Lab_Programs_java/Gale_Shapeley_Algorithm.java
@@ -1,97 +1,62 @@
-public class galeShapeley {
-    private int N, engagedCount;
-    private String[][] menPref;
-    private String[][] womenPref;
-    private String[] men;
-    private String[] women;
-    private String[] womenPartner;
-    private boolean[] menEngaged;
+import java.util.Arrays;
 
-    public galeShapeley(String[] m, String[] w, String[][] mp, String[][] wp){
-        N = mp.length;
-        engagedCount = 0;
-        men = m;
-        women = w;
-        menPref = mp;
-        womenPref = wp;
-        menEngaged = new boolean[N];
-        womenPartner = new String[N];
-        calcMatches();
-    }
-    //function to calculate all matches 
-    private void calcMatches(){
-        while (engagedCount < N){
-            int free;
-            for (free = 0; free < N; free++)
-                if (!menEngaged[free])
-                    break;
-            for (int i = 0; i < N && !menEngaged[free]; i++){
-                int index = womenIndexOf(menPref[free][i]);
-                if (womenPartner[index] == null){
-                    womenPartner[index] = men[free];
-                    menEngaged[free] = true;
-                    engagedCount++;
-                }else{
-                    String currentPartner = womenPartner[index];
-                    if (morePreference(currentPartner, men[free], index)){
-                        womenPartner[index] = men[free];
-                        menEngaged[free] = true;
-                        menEngaged[menIndexOf(currentPartner)] = false;
-                    }
-                }
-            }            
-        }
-        printCouples();
-    }
-    //function to check if women prefers new partner over old assigned partner
-    private boolean morePreference(String curPartner, String newPartner, int index){
-        for (int i = 0; i < N; i++){
-            if (womenPref[index][i].equals(newPartner))
+public class GaleShapley {
+    static int N = 3; // Number of men or women
+
+    static boolean prefers(int[][] prefer, int w, int m1, int m) {
+        for (int i = 0; i < N; i++) {
+            if (prefer[w][i] == m1) // If m1 comes before m in list of w, then w prefers m1
                 return true;
-            if (womenPref[index][i].equals(curPartner))
+            if (prefer[w][i] == m) // If m comes before m1 in list of w, then w prefers m
                 return false;
         }
         return false;
     }
-    // get men index 
-    private int menIndexOf(String str){
-        for (int i = 0; i < N; i++)
-            if (men[i].equals(str))
-                return i;
-        return -1;
-    }
-    // get women index 
-    private int womenIndexOf(String str){
-        for (int i = 0; i < N; i++)
-            if (women[i].equals(str))
-                return i;
-        return -1;
-    }
 
-    // print couples 
-    public void printCouples(){
-        System.out.println("Couples are : ");
-        for (int i = 0; i < N; i++){
-            System.out.println(womenPartner[i] +" "+ women[i]);
+    static void stableMatching(int[][] menPref, int[][] womenPref) {
+        int[] womenPartner = new int[N];
+        boolean[] freeMen = new boolean[N];// Keep track of free men
+        Arrays.fill(freeMen, true);
+        Arrays.fill(womenPartner, -1); // Initialize all women as free
+        int freeCount = N;
+
+        while (freeCount > 0) { // While there are free men who still have a woman to propose to
+            int m;
+            for (m = 0; m < N; m++) {
+                if (freeMen[m]) {
+                    break; }
+            }
+            for (int i = 0; i < N && freeMen[m]; i++) { // Find the first woman on this man's list who hasn't been proposed to yet
+                int w = menPref[m][i];
+
+                if (womenPartner[w] == -1) { // If the woman is free, pair m and w
+                    womenPartner[w] = m;
+                    freeMen[m] = false;
+                    freeCount--;
+                } else {
+                    int m1 = womenPartner[w];
+                    if (prefers(womenPref, w, m, m1)) { // If w prefers m over her current partner m1,
+                        womenPartner[w] = m; // then break the match between w and m1 and pair w with m
+                        freeMen[m] = false;
+                        freeMen[m1] = true; }
+                }
+            }
         }
+        System.out.println("Woman   Man");
+        for (int i = 0; i < N; i++) {
+            System.out.println(" " + i + "      " + womenPartner[i]);}
     }
-    public static void main(String[] args){
-        System.out.println("Gale Shapley Marriage Algorithm");
-        String[] m = {"M1", "M2", "M3", "M4", "M5"};
-        String[] w = {"W1", "W2", "W3", "W4", "W5"};
-        String[][] mp = {{"W5", "W2", "W3", "W4", "W1"}, 
-                         {"W2", "W5", "W1", "W3", "W4"}, 
-                         {"W4", "W3", "W2", "W1", "W5"}, 
-                         {"W1", "W2", "W3", "W4", "W5"},
-                         {"W5", "W2", "W3", "W4", "W1"}};                    
-
-        String[][] wp = {{"M5", "M3", "M4", "M1", "M2"}, 
-                         {"M1", "M2", "M3", "M5", "M4"}, 
-                         {"M4", "M5", "M3", "M2", "M1"},
-                         {"M5", "M2", "M1", "M4", "M3"}, 
-                         {"M2", "M1", "M4", "M3", "M5"}};
-
-        galeShapeley gs = new galeShapeley(m, w, mp, wp);              
-
+    public static void main(String[] args) {
+        int[][] menPref = {
+            {0, 1, 2},
+            {1, 0, 2},
+            {0, 1, 2},
+        };
+        int[][] womenPref = {
+            {0, 1, 2},
+            {1, 2, 0},
+            {2, 0, 1},
+        };
+        stableMatching(menPref, womenPref);
     }
 }
