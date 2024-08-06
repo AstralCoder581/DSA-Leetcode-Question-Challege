e78e40c48524e367156e8fff49147eaf25747d3e
Abhishek V K <160398136+AstralCoder581@users.noreply.github.com>
2024-08-06 19:17:46 +0530

    Update Bellmanford_Algorithm.java

--- Abhishek_V_K/Lab_Programs_java/Bellmanford_Algorithm.java
+++ Abhishek_V_K/Lab_Programs_java/Bellmanford_Algorithm.java
@@ -2,34 +2,29 @@ import java.util.*;
 
 public class bellmanFordAlgo {
     static class Edge{
-        int src;
-        int dest;
-        int wt;
-
+        int src, dest, wt;
         public Edge(int s, int d, int w){
             this.src = s;
             this.dest = d;
-            this.wt = w;
-        }
+            this.wt = w; }
     }
-
     public static void createGraph(ArrayList<Edge> graph[]){
-        for (int i=0; i<graph.length; i++){
-            graph[i] = new ArrayList<Edge>();
-        }
+        for (int i=0; i<graph.length; i++) graph[i] = new ArrayList<Edge>();
+        graph[0].add(new Edge(0,1,-4));
+        graph[0].add(new Edge(0,5,-3));
 
-        graph[0].add(new Edge(0,1,2));
-        graph[0].add(new Edge(0,2,4));
+        graph[1].add(new Edge(1,3,-1));
+        graph[1].add(new Edge(1,4,-2));
 
-        graph[1].add(new Edge(1,2,-4));
+        graph[2].add(new Edge(2,1,8));
+        graph[1].add(new Edge(2,5,3));
 
-        graph[2].add(new Edge(2,3,2));
+        graph[3].add(new Edge(3,0,6));
+        graph[1].add(new Edge(3,5,4));
 
-        graph[3].add(new Edge(3,4,4));
-
-        graph[4].add(new Edge(4,1,-1));
+        graph[4].add(new Edge(4,2,-3));
+        graph[1].add(new Edge(4,5,2));
     }
-
     public static void bellmanFord(ArrayList<Edge> graph[], int src, int V){
         int dist[] = new int[V]; //stores distance
         for (int i=0; i<V; i++){
@@ -37,47 +32,25 @@ public class bellmanFordAlgo {
                 dist[i] = Integer.MAX_VALUE; // all the nodes will be set to infinity apart from the source
             }
         }
-
         for( int k=0; k<V-1; k++){
             for(int i=0; i<V; i++){
                 for(int j=0; j<graph[i].size(); j++){
                    Edge e = graph[i].get(j);
                    int u = e.src;
                    int v = e.dest;
-                   
                    if (dist[u] != Integer.MAX_VALUE && dist[u]+e.wt < dist[v]) {
                         dist[v] = dist[u] + e.wt;
                    }
                 }
             }
         }
-        //Detect -ve weight cycle
-        for(int i=0; i<V; i++){
-            for(int j=0; j<graph[i].size(); j++){
-               Edge e = graph[i].get(j);
-               int u = e.src;
-               int v = e.dest;
-               
-               if (dist[u] != Integer.MAX_VALUE && dist[u]+e.wt < dist[v]) {
-                    System.out.println("Negative wt cycle");
-               }
-            }
-        }
-
-        for(int i=0; i<dist.length; i++){
-            System.out.print(dist[i]+" ");
-        }
+        for(int i=0; i<dist.length; i++) System.out.print(dist[i]+" ");
         System.out.println();
     }
-
     public static void main(String[] args) {
-        int V=5;
-        @SuppressWarnings("unchecked")
+        int V=6;
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
-        int src = 0;
-        bellmanFord(graph, src, V);
+        bellmanFord(graph, 0, V);
     }
 }
-
-
