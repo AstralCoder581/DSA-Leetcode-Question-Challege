3a7e8e6ba4a076d1fb265b5fbd91f13888a00b44
Abhishek V K <160398136+AstralCoder581@users.noreply.github.com>
2024-08-06 12:03:08 +0530

    Update Gale_Shapeley_Algorithm.java

--- Abhishek_V_K/Lab_Programs_java/Gale_Shapeley_Algorithm.java
+++ Abhishek_V_K/Lab_Programs_java/Gale_Shapeley_Algorithm.java
@@ -1,62 +1,64 @@
-import java.util.Arrays;
+import java.util.*;
 
-public class GaleShapley {
-    static int N = 3; // Number of men or women
-
-    static boolean prefers(int[][] prefer, int w, int m1, int m) {
-        for (int i = 0; i < N; i++) {
-            if (prefer[w][i] == m1) // If m1 comes before m in list of w, then w prefers m1
-                return true;
-            if (prefer[w][i] == m) // If m comes before m1 in list of w, then w prefers m
-                return false;
+public class stablematching {
+    static int N = 3; 
+    static boolean wPrefersM1OverM(int prefer[][], int w, int m, int m1){ // This function returns true if woman 'w' prefers man 'm1' over man 'm' 
+        // Check if w prefers m over her current engagement m1 
+        for (int i = 0; i < N; i++){ 
+            if (prefer[w][i] == m1) 
+                return true; 
+            if (prefer[w][i] == m) 
+            return false; 
         }
         return false;
-    }
-
-    static void stableMatching(int[][] menPref, int[][] womenPref) {
-        int[] womenPartner = new int[N];
-        boolean[] freeMen = new boolean[N];// Keep track of free men
-        Arrays.fill(freeMen, true);
-        Arrays.fill(womenPartner, -1); // Initialize all women as free
-        int freeCount = N;
-
-        while (freeCount > 0) { // While there are free men who still have a woman to propose to
-            int m;
-            for (m = 0; m < N; m++) {
-                if (freeMen[m]) {
-                    break; }
-            }
-            for (int i = 0; i < N && freeMen[m]; i++) { // Find the first woman on this man's list who hasn't been proposed to yet
-                int w = menPref[m][i];
-
-                if (womenPartner[w] == -1) { // If the woman is free, pair m and w
-                    womenPartner[w] = m;
-                    freeMen[m] = false;
-                    freeCount--;
-                } else {
-                    int m1 = womenPartner[w];
-                    if (prefers(womenPref, w, m, m1)) { // If w prefers m over her current partner m1,
-                        womenPartner[w] = m; // then break the match between w and m1 and pair w with m
-                        freeMen[m] = false;
-                        freeMen[m1] = true; }
+    } 
+ 
+    static void stableMarriage(int prefer[][]){ //Boys are numbered as 0 to N-1. Girls are numbered as N to 2N-1.
+        int wPartner[] = new int[N]; 
+        boolean mFree[] = new boolean[N]; // If mFree[i] is false, then man 'i' is free, otherwise engaged. 
+        Arrays.fill(wPartner, -1); //Initialize all men and women as free
+        int freeCount = N; 
+        while (freeCount > 0) { // While there are free men  
+            int m; 
+            for (m = 0; m < N; m++) 
+                if (mFree[m] == false) 
+                    break; 
+            for (int i = 0; i < N && mFree[m] == false; i++) { 
+                int w = prefer[m][i]; 
+                if (wPartner[w - N] == -1) { 
+                    wPartner[w - N] = m; 
+                    mFree[m] = true; 
+                    freeCount--; 
+                }else // If w is not free 
+                { 
+                    int m1 = wPartner[w - N]; 
+                    if (wPrefersM1OverM(prefer, w, m, m1) == false){ 
+                        wPartner[w - N] = m; 
+                        mFree[m] = true; 
+                        mFree[m1] = false; 
+                    } 
                 }
-            }
+            } 
         }
-        System.out.println("Woman   Man");
+        System.out.println("Woman Man"); 
         for (int i = 0; i < N; i++) {
-            System.out.println(" " + i + "      " + womenPartner[i]);}
-    }
-    public static void main(String[] args) {
-        int[][] menPref = {
-            {0, 1, 2},
-            {1, 0, 2},
-            {0, 1, 2},
-        };
-        int[][] womenPref = {
-            {0, 1, 2},
-            {1, 2, 0},
-            {2, 0, 1},
-        };
-        stableMatching(menPref, womenPref);
+            System.out.print(" "); 
+            System.out.println((i + N) + "	 " + wPartner[i]);
+        }
+    } 
+    // Driver Code
+    public static void main(String[] args) { 
+        int prefer[][] = new int[][]{{4, 5, 3}, 
+                                     {3, 5, 4}, 
+                                     {4, 5, 3}, 
+                                      
+                                     {0, 1, 2}, 
+                                     {0, 1, 2}, 
+                                     {0, 1, 2}, 
+                                    }; 
+        stableMarriage(prefer); 
     }
 }
+
+
+
