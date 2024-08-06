4bf8408614c6e14131a6601326f4273533b696de
Abhishek V K <160398136+AstralCoder581@users.noreply.github.com>
2024-08-06 12:32:46 +0530

    Update Merge_Sort.java

--- Abhishek_V_K/Lab_Programs_java/Merge_Sort.java
+++ Abhishek_V_K/Lab_Programs_java/Merge_Sort.java
@@ -7,9 +7,7 @@ public class mergesortProg{
 	}
 
 	public static void mergesort(int arr[], int si, int ei){
-		if(si >= ei){
-			return;
-		}
+		if(si >= ei) return;
 		int mid = si + (ei-si)/2;
 		mergesort(arr,si,mid);
 		mergesort(arr,mid+1,ei);
@@ -18,31 +16,14 @@ public class mergesortProg{
 
 	public static void merge(int arr[], int si, int mid, int ei){
 		int temp[] = new int[ei-si+1];
-		int i = si;
-		int j = mid+1;
-		int k = 0;
-
+		int i = si, j = mid+1, k = 0;
 		while(i <= mid && j <= ei){
-			if(arr[i] < arr[j]){
-				temp[k] = arr[i];
-				i++;
-			} else{
-				temp[k] = arr[j];
-				j++;
-			}
-			k++;
+			if(arr[i] < arr[j]) temp[k++] = arr[i++];
+			else temp[k++] = arr[j++];
 		}
-
-		while(i <= mid){
-			temp[k++] = arr[i++];
-		}
-		while(j <= ei){
-			temp[k++] = arr[j++];
-		}
-		
-		for( k= 0, i = si; k < temp.length; k++, i++){
-			arr[i] = temp[k];
-		}
+		while(i <= mid) temp[k++] = arr[i++];
+		while(j <= ei) temp[k++] = arr[j++];
+		for( k= 0, i = si; k < temp.length; k++, i++) arr[i] = temp[k];
 	}
 
 	public static void main(String args[]){
