0127e916bbf629cb78467f281aed107a8dd87c31
Abhishek V K <160398136+AstralCoder581@users.noreply.github.com>
2024-08-06 15:42:22 +0530

    Update weighted_Interval_Scheduling.java

--- Abhishek_V_K/Lab_Programs_java/weighted_Interval_Scheduling.java
+++ Abhishek_V_K/Lab_Programs_java/weighted_Interval_Scheduling.java
@@ -1,43 +1,38 @@
 import java.util.*;
 
-class Job {
-    int start, end, profit;
-
-    Job(int start, int end, int profit) {
+class Job implements Comparable<Job> {
+    int start, finish, profit;
+    Job(int start, int finish, int profit) {
         this.start = start;
-        this.end = end;
+        this.finish = finish;
         this.profit = profit;
+    }
+    @Override
+    public int compareTo(Job other) {
+        return this.finish - other.finish;
     }
 }
 
 public class weightedinterval {
-    
-    private static int findNonConflictingJob(Job[] jobs, int index) {
+    static int latestNonConflict(Job[] jobs, int index) {
         for (int j = index - 1; j >= 0; j--) {
-            if (jobs[j].end <= jobs[index].start) {
-                return j;
-            }
-        }
-        return -1;
+            if (jobs[j].finish <= jobs[index].start) {
+                return j; }
+        }return -1;
     }
 
-    public static int maximum(Job[] jobs) {
-        Arrays.sort(jobs, Comparator.comparingInt(job -> job.end));
-        int[] T = new int[jobs.length];
-        T[0] = jobs[0].profit;
-
-        for (int i = 1; i < jobs.length; i++) {
-            int includeProfit = jobs[i].profit;
-            int nonConflictingIndex = findNonConflictingJob(jobs, i);
-            if (nonConflictingIndex != -1) {
-                includeProfit += T[nonConflictingIndex];
-            }
-            T[i] = Math.max(includeProfit, T[i - 1]);
-        }
-        
-        return Arrays.stream(T).max().orElse(0);
+    static int findMaxProfit(Job[] jobs) {
+        Arrays.sort(jobs); // Sort jobs based on finish time
+        int n = jobs.length;
+        int[] dp = new int[n]; // dp[i] stores the maximum profit up to the i-th job
+        dp[0] = jobs[0].profit; // Base case: profit of the first job
+        for (int i = 1; i < n; i++) {
+            int inclProfit = jobs[i].profit; // Include current job
+            int l = latestNonConflict(jobs, i);
+            if (l != -1) inclProfit += dp[l];
+            dp[i] = Math.max(inclProfit, dp[i - 1]); // Store the maximum of including or excluding the current job
+        }return dp[n - 1]; // The last entry in dp[] contains the result
     }
-
     public static void main(String[] args) {
         Job[] jobs = {
             new Job(1, 3, 5),
@@ -47,7 +42,6 @@ public class weightedinterval {
             new Job(5, 8, 11),
             new Job(7, 9, 2)
         };
-        
-        System.out.println(maximum(jobs));
+        System.out.println(findMaxProfit(jobs));
     }
 }
