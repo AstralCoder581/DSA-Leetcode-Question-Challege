ff4aa2c9f634a89ba73998835cc98277dc927d44
Abhishek V K <160398136+AstralCoder581@users.noreply.github.com>
2024-08-06 12:30:26 +0530

    Update Counting_Inversion.java

--- Abhishek_V_K/Lab_Programs_java/Counting_Inversion.java
+++ Abhishek_V_K/Lab_Programs_java/Counting_Inversion.java
@@ -1,40 +1,34 @@
 import java.util.ArrayList;
 
 public class countinversion {
-
-    private static int merge(int[] arr, int low, int mid, int high) {
+    private static int merge(int[] arr, int si, int mid, int ei) {
         ArrayList<Integer> temp = new ArrayList<>();
-        int left = low, right = mid + 1;
-        int cnt = 0;
-        while (left <= mid && right <= high) {
-            if (arr[left] <= arr[right]) {
-                temp.add(arr[left++]);
+        int i = si, j = mid + 1, cnt = 0;
+        while (i <= mid && j <= ei) {
+            if (arr[i] < arr[j]) {
+                temp.add(arr[i++]);
             } else {
-                temp.add(arr[right++]);
-                cnt += (mid - left + 1); }
+                temp.add(arr[j++]);
+                cnt += (mid - i + 1); }
         }
-        while (left <= mid) temp.add(arr[left++]);
-        while (right <= high) temp.add(arr[right++]);
-        for (int i = low; i <= high; i++) arr[i] = temp.get(i - low);
+        while (i <= mid) temp.add(arr[i++]);
+        while (j <= ei) temp.add(arr[j++]);
+        for (int k = si; k <= ei; k++) arr[k] = temp.get(k - si);
         return cnt;
     }
 
-    private static int mergeSort(int[] arr, int low, int high) {
-        if (low >= high) return 0;
-        int mid = (low + high) / 2;
+    private static int mergeSort(int[] arr, int si, int ei) {
+        if (si >= ei) return 0;
+        int mid = (si + ei) / 2;
         int cnt = 0;
-        cnt += mergeSort(arr, low, mid);
-        cnt += mergeSort(arr, mid + 1, high);
-        cnt += merge(arr, low, mid, high);
+        cnt += mergeSort(arr, si, mid);
+        cnt += mergeSort(arr, mid + 1, ei);
+        cnt += merge(arr, si, mid, ei);
         return cnt;
     }
-
-    public static int numberOfInversions(int[] arr) {
-        return mergeSort(arr, 0, arr.length - 1);
-    }
-
     public static void main(String[] args) {
-        int[] a = {5, 4, 3, 2, 1};
-        System.out.println("The number of inversions are: " + numberOfInversions(a));
+        int[] a = {4, 3, 2, 1, 5, 6, 8, 7};
+        int numberOfInversions = mergeSort(a, 0, a.length - 1);
+        System.out.println("The number of inversions are: " + numberOfInversions);
     }
 }
