6b6e68f3788a262fbb8708905b6d5ce8431ea820
Abhishek V K <160398136+AstralCoder581@users.noreply.github.com>
2024-08-06 11:15:14 +0530

    Update Travel_Salesman_Problem.java

--- Abhishek_V_K/Lab_Programs_java/Travel_Salesman_Problem.java
+++ Abhishek_V_K/Lab_Programs_java/Travel_Salesman_Problem.java
@@ -1,33 +1,33 @@
 public class TravelSalesman {
     static int n = 5;
-    static int[][] distan = {
+    static int[][] dist = {
        {0, 2, 0, 12, 5},
        {2, 0, 4, 8, 0},
        {0, 4, 0, 3, 3},
        {12, 8, 3, 0, 10},
        {5, 0, 3, 10, 0}
     };
-    static int completed_visit = (1 << n) - 1;
+    static int visited = (1 << n) - 1;
     static int[][] DP = new int[32][8];
-    static int TSP(int mark, int position) {
-       if (mark == completed_visit) {
-          return distan[position][0];
+    static int TSP(int mark, int pos) {
+       if (mark == visited) {
+          return dist[pos][0];
        }
-       if (DP[mark][position] != -1) {
-          return DP[mark][position];
+       if (DP[mark][pos] != -1) {
+          return DP[mark][pos];
        }
-       int answer = Integer.MAX_VALUE;
+       int ans = Integer.MAX_VALUE;
        for (int city = 0; city < n; city++) {
           if ((mark & (1 << city)) == 0) {
-             int newAnswer = distan[position][city] + TSP(mark | (1 << city), city);
-             answer = Math.min(answer, newAnswer);
+             int newAns = dist[pos][city] + TSP(mark | (1 << city), city); //(1 << city) = 2^city
+             ans = Math.min(ans, newAns);
           }
        }
-       DP[mark][position] = answer;
-       return answer;
+       DP[mark][pos] = ans;
+       return ans;
     }
     public static void main(String[] args) {
-       for (int i = 0; i < (1 << n); i++) {
+       for (int i = 0; i < (1 << n); i++) { // (1 << n) = 2^n
           for (int j = 0; j < n; j++) {
               DP[i][j] = -1;
           }
